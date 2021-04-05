9b89332210d6e51b4af2fe7e842f0140d5cf8c3e
OxyTom <oxytom8@gmail.com>
2021-04-05 02:23:48 +0200

    preparing for deployement

---
+++ api/db/gdclobby.sql
@@ -0,0 +1,158 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 05 avr. 2021 à 00:15
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `gdclobby`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mission`
+--
+
+CREATE TABLE `mission` (
+  `MissionId` int(11) NOT NULL,
+  `Name` text NOT NULL,
+  `Date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `mission_teams`
+--
+
+CREATE TABLE `mission_teams` (
+  `teamTeamId` int(11) NOT NULL,
+  `missionMissionId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `player`
+--
+
+CREATE TABLE `player` (
+  `PlayerId` int(11) NOT NULL,
+  `Username` varchar(64) NOT NULL,
+  `Password` varchar(64) NOT NULL,
+  `IsAdmin` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE `role` (
+  `RoleId` int(11) NOT NULL,
+  `MissionId` int(11) NOT NULL,
+  `PlayerId` int(11) DEFAULT NULL,
+  `TeamId` int(11) DEFAULT NULL,
+  `Name` varchar(255) NOT NULL,
+  `IsBooked` tinyint(1) NOT NULL DEFAULT 0,
+  `Condi` text NOT NULL COMMENT 'Conditions selon lesquelles le slot est considéré comme “ouvert”'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `team`
+--
+
+CREATE TABLE `team` (
+  `TeamId` int(11) NOT NULL,
+  `Name` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `mission`
+--
+ALTER TABLE `mission`
+  ADD PRIMARY KEY (`MissionId`);
+
+--
+-- Index pour la table `mission_teams`
+--
+ALTER TABLE `mission_teams`
+  ADD PRIMARY KEY (`teamTeamId`,`missionMissionId`),
+  ADD KEY `missionMissionId` (`missionMissionId`);
+
+--
+-- Index pour la table `player`
+--
+ALTER TABLE `player`
+  ADD PRIMARY KEY (`PlayerId`),
+  ADD UNIQUE KEY `Name` (`Username`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`RoleId`),
+  ADD KEY `PlayerId` (`PlayerId`),
+  ADD KEY `TeamId` (`TeamId`),
+  ADD KEY `MissionId` (`MissionId`);
+
+--
+-- Index pour la table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`TeamId`),
+  ADD UNIQUE KEY `NameTeam` (`Name`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `mission`
+--
+ALTER TABLE `mission`
+  MODIFY `MissionId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `player`
+--
+ALTER TABLE `player`
+  MODIFY `PlayerId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+  MODIFY `RoleId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `team`
+--
+ALTER TABLE `team`
+  MODIFY `TeamId` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ api/src/ormconfig.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
+const config: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'db',
+  port: 3306,
+  username: 'root',
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  logging: ['error'],
+};
+
+export default config;

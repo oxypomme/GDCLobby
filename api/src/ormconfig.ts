d49dd38aafdba784d30964774c5cd9e89ffaa744
OxyTom <oxytom8@gmail.com>
2021-04-10 14:32:44 +0200

    ready to deploy

--- api/src/ormconfig.ts
+++ api/src/ormconfig.ts
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.MYSQL_HOST,
+  host: process.env.DB_HOST,
   port: 3306,
-  username: 'root',
-  password: process.env.MYSQL_ROOT_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   logging: ['error'],
